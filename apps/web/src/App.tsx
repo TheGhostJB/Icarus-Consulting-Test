409dfc9490a5323727fee8dc2c0325e60b9971d3
DanielaHG <A00838702@tec.mx>
2026-04-11 14:51:06 -0600

    chore: delete unnecessary  routes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -6,8 +6,6 @@ import PaySuccess from "./pages/paySuccess";
 import OffSeasonPage from "./pages/OffSeasonPage";
 import TeamPage from "./pages/TeamPage";
 import VoiceAgent from "./components/VoiceAgent/VoiceAgent";
-import { Signin } from "./components/auth/Signin";
-import { Signup } from "./components/auth/Signup";
 import PrivateRoute from "./components/auth/privateRoute";
 
 function App() {
@@ -19,7 +17,7 @@ function App() {
       <Route 
         path="/team" 
         element={
-          <PrivateRoute name="">
+          <PrivateRoute>
             <TeamPage />{''}
           </PrivateRoute>
         } 
@@ -28,8 +26,6 @@ function App() {
       <Route path="/paySuccess" element={<PaySuccess />} />
       <Route path="/offseason" element={<OffSeasonPage />} />
       <Route path="/voice-agent" element={<VoiceAgent />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }

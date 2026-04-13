53b8f8fd47147a9085d80fd48c38c73095c6a51a
Pablo Zapata <saldana_d@outlook.com>
2026-04-13 07:35:37 -0600

    Live chat V2 (Chat y Twitter Funcionando)

--- services/rooms-service/index.js
+++ services/rooms-service/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const { Pool } = require("pg");
 
 const app = express();
-app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 4003;
